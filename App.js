d9f465890b151200aec938c4f8374310496b4b81
rohitchebolu <chebolurohit@gmail.com>
2024-01-17 16:36:31 +0530

    Updated Header

--- App.js
+++ App.js
@@ -1,44 +1,36 @@
-const parent = React.createElement(
-    "div",
-    {id: "parent"},
-    [
-        React.createElement(
-            "div",
-            {id: "child"},
-            [React.createElement(
-                "h1",
-                {},
-                "I'm h1 tag"        
-            ),
-            React.createElement(
-                "h2",
-                {},
-                "I'm h2 tag"        
-            )
-            ]    
-        ),
-        React.createElement(
-            "div",
-            {id: "child2"},
-            [React.createElement(
-                "h1",
-                {},
-                "I'm h1 tag"        
-            ),
-            React.createElement(
-                "h2",
-                {},
-                "I'm h2 tag"        
-            )
-            ]    
-        )
-    ]
-)
+import React from "react";
+import ReactDOM from "react-dom";
 
-//const heading = React.createElement("h1", {id : "heading"}, "Hello from React!");
+const header = React.createElement("div", { className: "title" }, [
+  React.createElement("h1", {}, "I'm H1 tag"),
+  React.createElement("h2", {}, "I'm H1 tag"),
+  React.createElement("h3", {}, "I'm H1 tag"),
+]);
 
-console.log(parent) //object
+const header2 = (
+  <div className="title">
+    <h1>I'm a heading</h1>
+    <h2>I'm a heading</h2>
+    <h3>I'm a heading</h3>
+  </div>
+);
 
+const Header3 = () => {
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src="https://lh4.googleusercontent.com/pzcL7G25jch7H0Vpgm9NvY_C47dcs2jBCJ0rcTApLLOhOBgQ1M7zLyq3qCAJT3HLkuPq_6CECXpVtCmK8-6PA0lXDNAtPTixHiZahoomXOmEfxzMFs-REzysEaJ5tPaRAH0wtFclD1HD_cHC9c-5-q4" alt="Logo" />
+      </div>
+      <div className="search-bar-container">
+        <div className="search-bar">
+          <input type="text" placeholder="Search" />
+        </div>
+      </div>
+      <div className="user-icon">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJUcZP8UWywZOYwpOUMcf4S6-HmAxHMIx4OA&usqp=CAU" alt="User-icon"/>
+      </div>
+    </header>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(parent); //renders object to h1 tag
+root.render(<Header3 />);
