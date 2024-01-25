46c47848cc92385561fd761901db8dd48966b282
rohitchebolu <chebolurohit@gmail.com>
2024-01-25 16:14:58 +0530

    Added dynamic data from api link and search bar, top rated functionalities and shimmer ui for better ux to user

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,45 +1,66 @@
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockdata";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 const Body = () => {
-  const [listOfRestaurant, setListOfRestaurant] = useState(resList);
+  const [listOfRestaurant, setListOfRestaurant] = useState([]);
+  const [filterOfRestaurant, setFilterOfRestaurant] = useState([]);
 
-  return (
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.6868159&lng=83.2184815&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+
+    console.log(json);
+    setListOfRestaurant(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setFilterOfRestaurant(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+  //conditional rendering
+  return filterOfRestaurant.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
+      <div className="search-bar">
+        <input
+          id="input"
+          placeholder="search"
+          onInput={(event) => {
+            let inputValue = event.target.value;
+            console.log(inputValue);
+            const filteredRes = listOfRestaurant.filter((res) => {
+              const restaurantName = res.info.name.toLowerCase();
+              const inputLowerCase = inputValue.toLowerCase();
+
+              // Check if the restaurant name contains the sequence of alphabets entered
+              return restaurantName.includes(inputLowerCase);
+            });
+
+            setFilterOfRestaurant(filteredRes);
+          }}
+        />
+      </div>
       <div className="filter-btn">
         <button
           onClick={() => {
             const filteredList = listOfRestaurant.filter(
               (res) => res.info.avgRating > 4.3
             );
-            setListOfRestaurant(filteredList);
+            setFilterOfRestaurant(filteredList);
           }}
         >
           Top Rated
         </button>
-        <div className="search-bar">
-          <input id="input" placeholder="search"></input>
-          <button
-            onClick={() => {
-              let inputValue = document.getElementById("input").value;
-              console.log(inputValue);
-              const filteredRes = listOfRestaurant.filter((res) => {
-                const restaurantName = res.info.name.toLowerCase();
-                const inputLowerCase = inputValue.toLowerCase();
-
-                // Check if the restaurant name contains the sequence of alphabets entered
-                return restaurantName.includes(inputLowerCase);
-              });
-
-              setListOfRestaurant(filteredRes);
-            }}
-          >
-            enter
-          </button>
-        </div>
       </div>
       <div className="res-container">
-        {listOfRestaurant.map((restaurant) => (
+        {filterOfRestaurant.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
 
 const Header = () => {
+  const [userEntryBtn, setUserEntryBtn] = useState("login");
   return (
     <header>
       <div className="header">
@@ -13,6 +15,16 @@ const Header = () => {
             <li>About Us</li>
             <li>Contact Us</li>
             <li>Cart</li>
+            <button
+              className="login-btn"
+              onClick={() => {
+                userEntryBtn === "login"
+                  ? setUserEntryBtn("logout")
+                  : setUserEntryBtn("login");
+              }}
+            >
+              {userEntryBtn}
+            </button>
           </ul>
         </div>
       </div>
